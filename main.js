bd64f24cf0a40712ce4b646ef948a09d5637ffb8
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2023-03-31 00:40:38 -0300

    criada a função gerarJson

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-function ConverterDetalhes() {
-    const detalhes = document.getElementById("detalhes").value;
+function gerarJson() {
+
+    codigo = document.getElementById('codigo').value;
+    identificador = document.getElementById('identificador').value;
+    validacoes = document.getElementById('validacoes').value;
+    retorno = document.getElementById('retorno').value;
+    detalhes = converterDetalhes(document.getElementById('detalhes').value);
+    resultado = document.getElementById('resultado');
+
+    output = `, {
+    "codigo": ${codigo},
+    "identificador": "${identificador}",
+    ${validacoes != '' ? `"validacoes": ${validacoes},` : ''}
+    "retorno": "${retorno}",
+    "detalhes" "${detalhes}"
+}
+    `
+    resultado.value = output
+}
+
+function converterDetalhes(detalhes) {
     const output = detalhes.replace(/(?:\r\n|\r|\n)/g, '\\n');
-
-    alert(output);
-
+    return output
 }
